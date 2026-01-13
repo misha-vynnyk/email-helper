27d171d9ad112603576e667287e1ec1cf0ce7545
Mykhailo Vynnyk <mykhailo.vynnyk@MacBook-Air-Mykhailo-2.local>
2026-01-13 17:25:06 +0100

    cleanup: remove unused Desktop/Mobile toggle buttons
    - Remove non-functional screen size toggle from header
    - Remove selectedScreenSize from AppState (dead code)
    - Center tabs in header
    - Simplify TemplatePanel component

--- src/App/TemplatePanel/index.tsx
+++ src/App/TemplatePanel/index.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 
-import { MonitorOutlined, PhoneIphoneOutlined } from "@mui/icons-material";
-import { Box, Stack, SxProps, ToggleButton, ToggleButtonGroup, Tooltip } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 import { BlockLibrary } from "../../blockLibrary";
-import {
-  setSelectedScreenSize,
-  useSelectedMainTab,
-  useSelectedScreenSize,
-} from "../../contexts/AppState";
+import { useSelectedMainTab } from "../../contexts/AppState";
 import { EmailSenderProvider } from "../../emailSender/EmailSenderContext";
 import { ImageConverterPanel } from "../../imageConverter";
 import { TemplateLibrary } from "../../templateLibrary";
@@ -19,29 +14,6 @@ import MainTabsGroup from "./MainTabsGroup";
 
 export default function TemplatePanel() {
   const selectedMainTab = useSelectedMainTab();
-  const selectedScreenSize = useSelectedScreenSize();
-
-  let mainBoxSx: SxProps = {
-    height: "100%",
-  };
-  if (selectedScreenSize === "mobile") {
-    mainBoxSx = {
-      ...mainBoxSx,
-      margin: "32px auto",
-      width: 370,
-      height: 800,
-      boxShadow:
-        "rgba(33, 36, 67, 0.04) 0px 10px 20px, rgba(33, 36, 67, 0.04) 0px 2px 6px, rgba(33, 36, 67, 0.04) 0px 0px 1px",
-    };
-  }
-
-  const handleScreenSizeChange = (_: React.SyntheticEvent, value: "mobile" | "desktop") => {
-    if (value === "mobile" || value === "desktop") {
-      setSelectedScreenSize(value);
-    } else {
-      setSelectedScreenSize("desktop");
-    }
-  };
 
   const renderMainPanel = () => {
     switch (selectedMainTab) {
@@ -85,40 +57,13 @@ export default function TemplatePanel() {
         <Stack
           px={2}
           direction='row'
-          gap={2}
           width='100%'
-          justifyContent='space-between'
+          justifyContent='center'
           alignItems='center'
         >
-          <Stack
-            direction='row'
-            spacing={2}
-          >
-            <MainTabsGroup />
-          </Stack>
-          <Stack
-            direction='row'
-            spacing={2}
-          >
-            <ToggleButtonGroup
-              value={selectedScreenSize}
-              exclusive
-              size='small'
-              onChange={handleScreenSizeChange}
-            >
-              <ToggleButton value='desktop'>
-                <Tooltip title='Desktop view'>
-                  <MonitorOutlined fontSize='small' />
-                </Tooltip>
-              </ToggleButton>
-              <ToggleButton value='mobile'>
-                <Tooltip title='Mobile view'>
-                  <PhoneIphoneOutlined fontSize='small' />
-                </Tooltip>
-              </ToggleButton>
-            </ToggleButtonGroup>
-          </Stack>
+          <MainTabsGroup />
         </Stack>
+        <Box sx={{ minWidth: 40 }} />
       </Stack>
       <Box sx={{ height: "calc(100vh - 49px)", overflow: "hidden", minWidth: 370 }}>
         {renderMainPanel()}
--- src/contexts/AppState.tsx
+++ src/contexts/AppState.tsx
@@ -7,27 +7,18 @@ import { create } from "zustand";
 
 type AppState = {
   document: any;
-
   selectedMainTab: "email" | "blocks" | "templates" | "images";
-  selectedScreenSize: "desktop" | "mobile";
-
   samplesDrawerOpen: boolean;
 };
 
 const appStateStore = create<AppState>(() => ({
   document: {},
   selectedMainTab: "email",
-  selectedScreenSize: "desktop",
-
-  samplesDrawerOpen: false, // Hidden by default
+  samplesDrawerOpen: false,
 }));
 
 export function useDocument() {
   return appStateStore((s) => s.document);
-}
-
-export function useSelectedScreenSize() {
-  return appStateStore((s) => s.selectedScreenSize);
 }
 
 export function useSelectedMainTab() {
@@ -45,8 +36,4 @@ export function useSamplesDrawerOpen() {
 export function toggleSamplesDrawerOpen() {
   const samplesDrawerOpen = !appStateStore.getState().samplesDrawerOpen;
   return appStateStore.setState({ samplesDrawerOpen });
-}
-
-export function setSelectedScreenSize(selectedScreenSize: AppState["selectedScreenSize"]) {
-  return appStateStore.setState({ selectedScreenSize });
 }
